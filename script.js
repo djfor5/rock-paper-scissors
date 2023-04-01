0c8042bf599ae57f04062cea9daabd6b1fd1cc8d
djfor5 <117873331+djfor5@users.noreply.github.com>
2023-04-01 17:01:15 +1100

    Add game function to script.js
    Add game function to play 5 rounds of rock, paper, scissors. Display
    the cumulative scores on the console after each round, then display
    the final scores after 5 rounds.

--- script.js
+++ script.js
@@ -20,27 +20,73 @@ getComputerChoice()
 
 
 function playRound(playerSelection, computerSelection){
-  if (playerSelection.toLowerCase() === 'rock' && computerSelection === "rock"){
-    return "Tie! Rock ties with Rock"
-  } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === "paper"){
-    return "You Lose! Rock loses to Paper"
-  } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === "scissors"){
-    return "You Win! Rock beats Scissors"
-  } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === "rock"){
-    return "You Win! Paper beats Rock"
-  } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === "paper"){
-    return "Tie! Paper ties with Paper"
-  } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === "scissors"){
-    return "You Lose! Paper loses to Scissors"
-  } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === "rock"){
-    return "You Lose! Scissors loses to Rock"
-  } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === "paper"){
-    return "You Win! Scissors beats Paper"
-  } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === "scissors"){
-    return "Tie! Scissors ties with Scissors"
+  playerSelection = playerSelection.toLowerCase()
+  computerSelection = computerSelection.toLowerCase()
+
+  if (playerSelection === 'rock' && computerSelection === "rock"){
+    // return "Tie! Rock ties with Rock"
+    return 0
+  } else if (playerSelection === 'rock' && computerSelection === "paper"){
+    // return "You Lose! Rock loses to Paper"
+    return -1
+  } else if (playerSelection === 'rock' && computerSelection === "scissors"){
+    // return "You Win! Rock beats Scissors"
+    return 1
+  } else if (playerSelection === 'paper' && computerSelection === "rock"){
+    // return "You Win! Paper beats Rock"
+    return 1
+  } else if (playerSelection === 'paper' && computerSelection === "paper"){
+    // return "Tie! Paper ties with Paper"
+    return 0
+  } else if (playerSelection === 'paper' && computerSelection === "scissors"){
+    // return "You Lose! Paper loses to Scissors"
+    return -1
+  } else if (playerSelection === 'scissors' && computerSelection === "rock"){
+    // return "You Lose! Scissors loses to Rock"
+    return -1
+  } else if (playerSelection === 'scissors' && computerSelection === "paper"){
+    // return "You Win! Scissors beats Paper"
+    return 1
+  } else if (playerSelection === 'scissors' && computerSelection === "scissors"){
+    // return "Tie! Scissors ties with Scissors"
+    return 0
   } 
 }
 
-const playerSelection = 'rock'
-const computerSelection = getComputerChoice()
-console.log(playRound(playerSelection, computerSelection))
+
+function game(){
+  let gameCount = 0
+  let playerScore = 0
+  let computerScore = 0
+
+  while (gameCount < 5){
+    console.log(`Round ${gameCount + 1}`)
+    let playerSelection
+  
+    while (playerSelection !== 'rock' && playerSelection !== 'paper' && playerSelection !== 'scissors'){
+      playerSelection = prompt("Enter 'rock', 'paper', or 'scissors': ").toLowerCase()
+    }
+    console.log(`Player selection: ${playerSelection}`)
+  
+    const computerSelection = getComputerChoice()
+    console.log(`Computer selection: ${computerSelection}`)
+    
+    let gameResult = playRound(playerSelection, computerSelection)
+    if (gameResult === 1) {
+      playerScore++
+      console.log(`You win round ${gameCount + 1}`)
+    } else if (gameResult === -1){
+      computerScore++
+      console.log(`You lose round ${gameCount + 1}`)
+    } else {
+      console.log(`You tied round ${gameCount + 1}`)
+    }
+    gameCount++
+
+    console.log(`Current scores: player score = ${playerScore}, computer score = ${computerScore}`)
+
+    console.log('\n')
+  }
+  console.log(`Final scores: player score = ${playerScore}, computer score = ${computerScore}`)
+}
+game()
