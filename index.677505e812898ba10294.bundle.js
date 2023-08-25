e34c4af181229c777dbc8cde64395dd7f8bf01be
djfor5 <117873331+djfor5@users.noreply.github.com>
2023-08-25 10:00:07 +1000

    Install Babel, update Webpack config

--- index.677505e812898ba10294.bundle.js
+++ index.677505e812898ba10294.bundle.js
@@ -2,6 +2,74 @@
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
+/***/ "./src/getComputerChoice.js":
+/*!**********************************!*\
+  !*** ./src/getComputerChoice.js ***!
+  \**********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   getComputerChoice: () => (/* binding */ getComputerChoice)
+/* harmony export */ });
+function getComputerChoice() {
+  const computerChoiceIndex = Math.floor(Math.random() * 3);
+  let computerDecision;
+  switch (computerChoiceIndex) {
+    case 0:
+      computerDecision = "ROCK";
+      break;
+    case 1:
+      computerDecision = "PAPER";
+      break;
+    case 2:
+      computerDecision = "SCISSORS";
+      break;
+    default:
+  }
+  return computerDecision;
+}
+
+
+/***/ }),
+
+/***/ "./src/playRound.js":
+/*!**************************!*\
+  !*** ./src/playRound.js ***!
+  \**************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   playRound: () => (/* binding */ playRound)
+/* harmony export */ });
+function playRound(playerSelection, computerSelection) {
+  // eslint-disable-line consistent-return
+  const playerSelectionUpper = playerSelection.toUpperCase();
+  const computerSelectionUpper = computerSelection.toUpperCase();
+  switch (true) {
+    // cases where player beats computer
+    case playerSelectionUpper === "ROCK" && computerSelectionUpper === "SCISSORS":
+    case playerSelectionUpper === "PAPER" && computerSelectionUpper === "ROCK":
+    case playerSelectionUpper === "SCISSORS" && computerSelectionUpper === "PAPER":
+      return 1;
+    // cases where player ties with computer
+    case playerSelectionUpper === "ROCK" && computerSelectionUpper === "ROCK":
+    case playerSelectionUpper === "PAPER" && computerSelectionUpper === "PAPER":
+    case playerSelectionUpper === "SCISSORS" && computerSelectionUpper === "SCISSORS":
+      return 0;
+    // cases where player loses to computer
+    case playerSelectionUpper === "ROCK" && computerSelectionUpper === "PAPER":
+    case playerSelectionUpper === "PAPER" && computerSelectionUpper === "SCISSORS":
+    case playerSelectionUpper === "SCISSORS" && computerSelectionUpper === "ROCK":
+      return -1;
+    default:
+  }
+}
+
+
+/***/ }),
+
 /***/ "./node_modules/css-loader/dist/cjs.js!./src/style.css":
 /*!*************************************************************!*\
   !*** ./node_modules/css-loader/dist/cjs.js!./src/style.css ***!
@@ -478,78 +546,6 @@ function styleTagTransform(css, styleElement) {
 }
 module.exports = styleTagTransform;
 
-/***/ }),
-
-/***/ "./src/getComputerChoice.js":
-/*!**********************************!*\
-  !*** ./src/getComputerChoice.js ***!
-  \**********************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-__webpack_require__.r(__webpack_exports__);
-/* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   getComputerChoice: () => (/* binding */ getComputerChoice)
-/* harmony export */ });
-function getComputerChoice() {
-  const computerChoiceIndex = Math.floor(Math.random() * 3);
-  let computerDecision;
-  switch (computerChoiceIndex) {
-    case 0:
-      computerDecision = "ROCK";
-      break;
-    case 1:
-      computerDecision = "PAPER";
-      break;
-    case 2:
-      computerDecision = "SCISSORS";
-      break;
-    default:
-  }
-  return computerDecision;
-}
-
-
-
-
-/***/ }),
-
-/***/ "./src/playRound.js":
-/*!**************************!*\
-  !*** ./src/playRound.js ***!
-  \**************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-__webpack_require__.r(__webpack_exports__);
-/* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   playRound: () => (/* binding */ playRound)
-/* harmony export */ });
-function playRound(playerSelection, computerSelection) { // eslint-disable-line consistent-return
-  const playerSelectionUpper = playerSelection.toUpperCase();
-  const computerSelectionUpper = computerSelection.toUpperCase();
-
-  switch (true) {
-    // cases where player beats computer
-    case playerSelectionUpper === "ROCK" && computerSelectionUpper === "SCISSORS":
-    case playerSelectionUpper === "PAPER" && computerSelectionUpper === "ROCK":
-    case playerSelectionUpper === "SCISSORS" && computerSelectionUpper === "PAPER":
-      return 1;
-    // cases where player ties with computer
-    case playerSelectionUpper === "ROCK" && computerSelectionUpper === "ROCK":
-    case playerSelectionUpper === "PAPER" && computerSelectionUpper === "PAPER":
-    case playerSelectionUpper === "SCISSORS" && computerSelectionUpper === "SCISSORS":
-      return 0;
-    // cases where player loses to computer
-    case playerSelectionUpper === "ROCK" && computerSelectionUpper === "PAPER":
-    case playerSelectionUpper === "PAPER" && computerSelectionUpper === "SCISSORS":
-    case playerSelectionUpper === "SCISSORS" && computerSelectionUpper === "ROCK":
-      return -1;
-    default:
-  }
-}
-
-
-
-
 /***/ })
 
 /******/ 	});
@@ -638,37 +634,31 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
-
 let gameCount = 0;
 let playerScore = 0;
 let computerScore = 0;
 let ties = 0;
-
 const SCORE_TO_WIN = 5;
-
 const round = document.querySelector("#round");
 const playerChoice = document.querySelector("#playerChoice");
 const computerChoice = document.querySelector("#computerChoice");
 const roundOutcome = document.querySelector("#roundOutcome");
 const roundWinner = document.querySelector("#roundWinner");
 const currentScores = document.querySelector("#currentScores");
-
 const btns = document.querySelectorAll("button");
-btns.forEach((btn) => {
-  btn.addEventListener("click", (event) => {
+btns.forEach(btn => {
+  btn.addEventListener("click", event => {
     const playerSelection = event.target.id.toUpperCase();
     // eslint-disable-next-line no-use-before-define
     const computerSelection = (0,_getComputerChoice_js__WEBPACK_IMPORTED_MODULE_1__.getComputerChoice)();
     // eslint-disable-next-line no-use-before-define
     const gameResult = (0,_playRound_js__WEBPACK_IMPORTED_MODULE_2__.playRound)(playerSelection, computerSelection);
-
     round.textContent = `ROUND ${gameCount + 1}`;
     playerChoice.textContent = `Player selection = ${playerSelection}`;
     computerChoice.textContent = `Computer selection = ${computerSelection}`;
     console.log(`%c${round.textContent}`, "font-weight: bold");
     console.log(playerChoice.textContent);
     console.log(computerChoice.textContent);
-
     if (gameResult === 1) {
       playerScore++;
       roundOutcome.textContent = `${playerSelection} beats ${computerSelection}`;
@@ -689,26 +679,23 @@ btns.forEach((btn) => {
       console.log(`%c${roundWinner.textContent}`, "font-weight: bold; font-style: italic");
     }
     gameCount++;
-
     currentScores.textContent = `Current scores: player score = ${playerScore}, computer score = ${computerScore}, ties = ${ties}, games played = ${gameCount}`;
     console.log(`Current scores: player score = ${playerScore}, computer score = ${computerScore}, ties = ${ties}, games played = ${gameCount}`);
     console.log("\n");
-
     if (playerScore === SCORE_TO_WIN || computerScore === SCORE_TO_WIN) {
       console.log("%cFINAL SCORES", "font-weight: bold");
       console.table({
         "Player score": playerScore,
         "Computer score": computerScore,
         "Tied games": ties,
-        "Games played": gameCount,
+        "Games played": gameCount
       });
       currentScores.textContent = `Final scores: player score = ${playerScore}, computer score = ${computerScore}, ties = ${ties}, games played = ${gameCount}`;
     }
   });
 });
-
 })();
 
 /******/ })()
 ;
-//# sourceMappingURL=index.2edae3cb9bc3e63546af.bundle.js.map
+//# sourceMappingURL=index.677505e812898ba10294.bundle.js.map
